27900217f377fa820db46607c5fbe86878bf23c6
Charith Miriharnga <charithmihiranga453@gmail.com>
2025-09-05 20:26:27 +0530

    tabs section file create.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { AuthProvider } from '@/context/AuthContext'
 
-const _layout = () => {
+const StackLayout = () => {
   return <Stack screenOptions={{headerShown: false}}></Stack>
 }
 
-export default _layout
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <StackLayout />
+    </AuthProvider>
+  )
+}
