9e7cd8070fa58d0d635585d395fb0c0cc2ffd753
Charith Miriharnga <charithmihiranga453@gmail.com>
2025-09-11 13:05:38 +0530

    profileModel File create.

---
+++ app/(modals)/profileModel.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ProfileModel = () => {
+  return (
+    <View>
+      <Text>ProfileModel</Text>
+    </View>
+  )
+}
+
+export default ProfileModel
