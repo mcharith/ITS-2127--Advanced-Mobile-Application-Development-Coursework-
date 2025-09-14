2e4397ded14d6891a43336a510ea3b5d3c5c9b6c
Charith Miriharnga <charithmihiranga453@gmail.com>
2025-09-14 18:54:02 +0530

    wallet service delete function done.

--- service/walletService.ts
+++ service/walletService.ts
@@ -1,6 +1,6 @@
 import { ResponseType, WalletType } from "@/types";
 import { uploadFileToCloudinary } from "./imageService";
-import { collection, deleteDoc, doc, setDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc, where, writeBatch } from "firebase/firestore";
 import { db } from "@/firebase";
 
 
@@ -45,14 +45,56 @@ export const createOrUpdateWallet = async (
   }
 };
 
-export const deleteWallet = async (walletId: string) : Promise<ResponseType> => {
-  try{
-    const walletRef = doc(db,"wallets",walletId);
+export const deleteWallet = async (walletId: string): Promise<ResponseType> => {
+  try {
+    const walletRef = doc(db, "wallets", walletId);
+
+    await deleteTransactionByWalletId(walletId);
+
     await deleteDoc(walletRef);
 
-    return {success: true, msg: "Wallet deleted successfully."}
-  }catch(err:any){
-    console.log('error deleting wallet:',err);
-    return {success: false, msg:err.message}
+    return { success: true, msg: "Wallet deleted successfully." };
+  } catch (err: any) {
+    console.log("error deleting wallet:", err);
+    return { success: false, msg: err.message };
   }
-}
+};
+
+export const deleteTransactionByWalletId = async (
+  walletId: string
+): Promise<ResponseType> => {
+  try {
+    let hasMoreTransaction = true;
+
+    while (hasMoreTransaction) {
+      const transactionQuery = query(
+        collection(db, "transactions"),
+        where("walletId", "==", walletId)
+      );
+
+      const transactionSnapshot = await getDocs(transactionQuery);
+
+      if (transactionSnapshot.empty) {
+        hasMoreTransaction = false;
+        break;
+      }
+
+      const batch = writeBatch(db);
+
+      transactionSnapshot.forEach((transactionDoc) => {
+        batch.delete(transactionDoc.ref);
+      });
+
+      await batch.commit();
+
+      console.log(
+        `${transactionSnapshot.size} transactions deleted in this batch.`
+      );
+    }
+
+    return { success: true, msg: "All transactions deleted successfully." };
+  } catch (err: any) {
+    console.log("error deleting transactions by walletId:", err);
+    return { success: false, msg: err.message };
+  }
+};
